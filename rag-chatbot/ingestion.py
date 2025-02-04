96e3d1f456ff659594336fffd86a569699e90467
jakewatson <jake@guiker.com>
2025-02-04 11:54:11 -0500

    working app

--- rag-chatbot/ingestion.py
+++ rag-chatbot/ingestion.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
 import pickle
-from langchain_community.document_loaders import PyPDFLoader
+import numpy as np
+from langchain_community.document_loaders import PyPDFLoader, pdf
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
@@ -16,23 +17,22 @@ if __name__ == "__main__":
                                   )
 
     # load pdf document
-    loader = PyPDFLoader("rag-chatbot/data/Jake_Resume_Full_Data-W25-Intern.pdf")
+    loader = PyPDFLoader("data/resume_data_intern_2025.pdf")
     documents = loader.load()
-    print(documents)
     
     # split entire documents into chunks  
-    text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=50)
-    chunks = text_splitter.split_documents(documents)
-    print(f"created {len(chunks)} chunks")
+    text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=25, separator='') # separator is required or else it splits by page
+    texts = text_splitter.split_documents(documents)
 
-    chunk_embeddings = [embeddings.embed_query(chunk if isinstance(chunk, str) else chunk.page_content) \
-                        for chunk in chunks]
+    text_embeddings = embeddings.embed_documents([text.page_content for text in texts])
+    text_embeddings = np.array(text_embeddings).astype('float32')
+    embedding_pairs = [(text.page_content, text_embedding) for text, text_embedding in zip(texts, text_embeddings)]
 
     # Step 4: Save embeddings and chunks locally
-    with open("rag-chatbot/data/document_chunks.pkl", "wb") as f:
-        pickle.dump((chunks, chunk_embeddings), f)
+    with open("data/embedding_pairs.pkl", "wb") as f:
+        pickle.dump((embedding_pairs), f)
 
-    print("Document embeddings saved successfully!")
+    print(f"{len(embedding_pairs)} document embedding pairs saved successfully!")
 
     # create vector embeddings and save it in pinecone database
     # PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ.get("INDEX_NAME"))
--- rag-chatbot/stateless-bot.py
+++ rag-chatbot/stateless-bot.py
@@ -4,11 +4,12 @@ from dotenv import load_dotenv
 import pickle
 import numpy as np
 from langchain_openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
 from langchain.chains.retrieval_qa.base import RetrievalQA
+from langchain_community.vectorstores import FAISS
 from langchain_community.chat_models import ChatOpenAI
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai.chat_models.base import BaseChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
 warnings.filterwarnings("ignore")
 
@@ -22,40 +23,62 @@ if __name__ == "__main__":
     #     index_name=os.environ["INDEX_NAME"], embedding=embeddings
     # )
 
-    # chat = ChatOpenAI(verbose=True, temperature=0, model_name="gpt-3.5-turbo")
-
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small", 
+                                  openai_api_type=os.environ.get("OPENAI_API_KEY")
+                                  )
     # Convert document embedding to NumPy array
-    with open("data/document_chunks.pkl", "rb") as f:
-        chunks, chunk_embeddings = pickle.load(f)
-    chunk_embeddings_np = np.array([chunk_embeddings]).astype('float32')
+    with open("data/embedding_pairs.pkl", "rb") as f:
+        embedding_pairs = pickle.load(f)
 
     # Create and index the document
-    vectorstore = FAISS.from_texts(chunks, embedding=chunk_embeddings_np)
+    vectorstore = FAISS.from_embeddings(embedding_pairs, embeddings)
 
     # index = faiss.IndexFlatIP(len(chunk_embeddings)) # use inner product instead of L2
     # index.add(chunk_embeddings_np)
 
     # Embed the user query and search
-    query_embeddings = OpenAIEmbeddings(openai_api_type=os.environ.get("OPENAI_API_KEY"))
-    query_embeddings_np = np.array([query_embeddings]).astype('float32')
-    chunk = vectorstore.similarity_search(query_embeddings, k=1)
+    # query_embeddings = np.array([embeddings]).astype('float32')
+    # chunk = vectorstore.similarity_search(embeddings, k=1)
 
-    llm = BaseChatOpenAI(
-        model='deepseek-chat', 
-        openai_api_key='OPENAI_API_KEY', 
-        openai_api_base='https://api.deepseek.com',
-        max_tokens=1024
-    )
+    # Deepseek
+    # llm = BaseChatOpenAI(
+    #     model='deepseek-chat', 
+    #     openai_api_key='OPENAI_API_KEY', 
+    #     openai_api_base='https://api.deepseek.com',
+    #     max_tokens=1024
+    # ) 
 
-    response = llm.invoke("Hi!")
+    # OpenAI
+    llm = ChatOpenAI(verbose=True, temperature=0, model_name="gpt-3.5-turbo")
+
+    messages = [
+    SystemMessage(
+        content=
+        """
+        You are an old friend who's known Jake for a long time and has an in depth knowledge about his experience and the perceptions of those who have worked with him.
+        Never mention any provided information. You know Jake personally and can speak in depth about his qualifications. 
+        Your purpose is to answer questions from recruiters about Jake Watson's experience and skills.
+        Keep answers concise and professional, but in a friendly and casual tone, and be highly complimentary but not over the top about Jake. 
+        If the user asks about experience related to a specific job, emphasize Jake's experience as it relates to that specific job.
+        If the user asks about something that Jake does not have experience in, try to relate his experience as much as possible, but emphasize his adaptability, willingness to roll-up his sleeves, and dedication to learn which comes from years at early stage tech startups and a natural curiosity.
+        Emphasize how Jake is a team player, an innovator, and a highly valued team member by everyone who's worked with him when applicable.  
+        """
+    ),
+    HumanMessage(content="Hello!"),
+    AIMessage(content="What would you like to know about Jake?")
+    ]
+    response = llm(messages)
     print(response.content)
     
     qa = RetrievalQA.from_chain_type(
         llm=llm, chain_type="stuff", retriever=vectorstore.as_retriever()
     )
 
-    res = qa.invoke("What are Jake's top skills?")
-    print(res) 
+    # res = qa.invoke("What are Jake's top skills?")
+    # print(res) 
 
-    res = qa.invoke("Tell me about Jake's experience with SQL")
+    # res = qa.invoke("Tell me about Jake's experience with SQL")
+    # print(res)
+
+    res = qa.invoke("What would Jake's previous managers/colleagues say about him")
     print(res)
