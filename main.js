573e90767aa1d6dea4996d8b51a08ac1854e103f
jakewatson <jake@guiker.com>
2025-02-05 12:31:23 -0500

    updates

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-document.getElementById('theme-toggle').addEventListener('click', function () {
-  const htmlElement = document.documentElement;
+const themeToggle = document.getElementById('theme-toggle');
 
-  if (htmlElement.classList.contains('dark-mode')) {
-      // Switch to light mode
-      htmlElement.classList.remove('dark-mode');
-      htmlElement.style.setProperty('--bg-color', '#f0efea');
-      htmlElement.style.setProperty('--text-color', '#141413');
-  } else {
-      // Switch to dark mode
-      htmlElement.classList.add('dark-mode');
-      htmlElement.style.setProperty('--bg-color', '#141413');
-      htmlElement.style.setProperty('--text-color', '#f0efea');
-  }
+themeToggle.addEventListener('change', function () {
+    const htmlElement = document.documentElement;
+
+    if (themeToggle.checked) {
+        // Enable dark mode
+        htmlElement.classList.add('dark-mode');
+        htmlElement.style.setProperty('--bg-color', '#141413');
+        htmlElement.style.setProperty('--text-color', '#f0efea');
+    } else {
+        // Enable light mode
+        htmlElement.classList.remove('dark-mode');
+        htmlElement.style.setProperty('--bg-color', '#f0efea');
+        htmlElement.style.setProperty('--text-color', '#141413');
+    }
 });
